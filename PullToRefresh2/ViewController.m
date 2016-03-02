cd828e5807ee26118cb59785a80f446a788eec3e
thanksdanny <hj-07@163.com>
2016-03-03 03:03:41 +0800

    添加导航栏

--- PullToRefresh2/ViewController.m
+++ PullToRefresh2/ViewController.m
@@ -11,10 +11,33 @@
 @interface ViewController () <UITableViewDelegate, UITableViewDataSource>
 
 @property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) UIRefreshControl *refreshControl;
+@property (nonatomic, strong) UINavigationBar *navBar;
 
 @end
 
 @implementation ViewController
+
+- (UINavigationBar *)navBar {
+    if (!_navBar) {
+        _navBar = [[UINavigationBar alloc] initWithFrame:CGRectMake(0, 0, 375, 64)];
+    }
+    return _navBar;
+}
+
+- (UIRefreshControl *)refreshControl {
+    if (!_refreshControl) {
+        _refreshControl = [[UIRefreshControl alloc] init];
+    }
+    return _refreshControl;
+}
+#pragma mark - action
+- (void)refreshEmoji {
+    NSLog(@"%s", __func__);
+    self.refreshControl.attributedTitle = [[NSAttributedString alloc] initWithString:@"下拉刷新"];
+    [self.tableView reloadData];
+    [self.refreshControl endRefreshing];
+}
 
 #pragma mark - 数据源
 - (NSArray *)currentEmoji {
@@ -50,13 +73,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
+    
+    // tableview
     self.tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
     [self.tableView registerClass:[UITableViewCell self] forCellReuseIdentifier:@"MyCells"]; // 没有这句程序一直蹦，但看别的demo又没用这个，有点奇怪
     self.tableView.delegate = self;
     self.tableView.dataSource = self;
-    self.tableView.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
+//    self.tableView.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
+    
+    // refreshControl
+//    self.refreshControl = [[UIRefreshControl alloc] init];
+    [self.refreshControl addTarget:self action:@selector(refreshEmoji) forControlEvents:UIControlEventValueChanged];
+    self.refreshControl.backgroundColor = [UIColor colorWithRed:0.113 green:0.113 blue:0.145 alpha:1];
+    self.refreshControl.tintColor = [UIColor redColor];
+    
+    // nav
+    self.navBar.barStyle = UIBarStyleBlackTranslucent;
+
     
     [self.view addSubview:self.tableView];
+    [self.view addSubview:self.navBar];
 }
 
 - (void)didReceiveMemoryWarning {
