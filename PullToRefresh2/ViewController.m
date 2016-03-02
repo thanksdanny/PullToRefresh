25b3a810430a3e8c7308056847454aadaf677e65
thanksdanny <hj-07@163.com>
2016-03-03 02:21:58 +0800

    初步完成数据源，与基本界面

--- PullToRefresh2/ViewController.m
+++ PullToRefresh2/ViewController.m
@@ -8,20 +8,59 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property (nonatomic, strong) UITableView *tableView;
 
 @end
 
 @implementation ViewController
 
+#pragma mark - 数据源
+- (NSArray *)currentEmoji {
+    return @[@"🤗🤗🤗🤗🤗", @"😅😅😅😅😅", @"😆😆😆😆😆"];
+}
+
+- (NSArray *)newEmoji {
+    return @[@"🏃🏃🏃🏃🏃", @"💩💩💩💩💩", @"👸👸👸👸👸", @"🤗🤗🤗🤗🤗", @"😅😅😅😅😅", @"😆😆😆😆😆"];
+}
+
+#pragma mark - tableview
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = nil;
+    static NSString *tableViewCellIdentifier = @"MyCells";
+    if (cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:tableViewCellIdentifier];
+    }
+    cell = [tableView dequeueReusableCellWithIdentifier:tableViewCellIdentifier];
+    cell.textLabel.text = [NSString stringWithFormat:@"%@", [self currentEmoji][indexPath.row]];
+    
+    return cell;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [[self currentEmoji] count];
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+#pragma mark - view
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+    self.tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+    [self.tableView registerClass:[UITableViewCell self] forCellReuseIdentifier:@"MyCells"]; // 没有这句程序一直蹦，但看别的demo又没用这个，有点奇怪
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    self.tableView.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
+    
+    [self.view addSubview:self.tableView];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
